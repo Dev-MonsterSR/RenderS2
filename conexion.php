624ed241aad678c58741ebf95b5d8c364170577c
Juan Angel <juan.urcia@tecsup.edu.pe>
2026-03-25 11:15:10 -0500

    second commit

--- conexion.php
+++ conexion.php
@@ -4,17 +4,20 @@ function conexion(){
     $host = "host=dpg-d71v6ovdiees73c23ol0-a.oregon-postgres.render.com";
     $port = "port=5432";
     $dbname = "dbname=test_evpi";
-    $username = "user=test_evpi_user";
+    $user = "user=test_evpi_user";
     $password = "password=gL9QBEDzoZ1RQkhHPfLDMjMXsdbPUK3i";
 
 
-    $db = pg_connect("host=$host port=$port dbname=$dbname user=$username password=$password");
-    if (!$db) {
-        echo "Error: No se pudo conectar a la base de datos.";
-        exit;
+    $connStr = "host=$host port=$port dbname=$dbname user=$user password=$password";
+
+        $db = pg_connect($connStr);
+
+        if (!$db) {
+            die("Error de conexión: " . pg_last_error());
+        }
+
+        return $db;
     }
-    return $db;
-}
 
 
 ?>
